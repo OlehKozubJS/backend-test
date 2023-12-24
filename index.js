011429bcdf718cd799470195e4dbb6e59ed5cb97
OlehKozubJS <oleg.kozub54@gmail.com>
2023-12-24 13:54:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ const addToStatistics = async (request, response, next) => {
   try {
     const { url, method } = request;
     const oldStatsData = await readFile("./db/statistics.txt", "utf-8");
-    const newStatsData = `date: ${new Date().toString()}, url: ${url}, method: ${method}`;
+    const newStatsData = {
+      date: new Date().toString(),
+      url: url,
+      method: method,
+    };
     await writeFile(
       "./db/statistics.txt",
       `${oldStatsData}\n \n${newStatsData}`
