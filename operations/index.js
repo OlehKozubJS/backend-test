fb3fe67e2959a0b901e5e7dba0677fd558ce1991
OlehKozubJS <oleg.kozub54@gmail.com>
2023-12-26 00:16:43 +0200

    Create index.js

---
+++ operations/index.js
@@ -0,0 +1,5 @@
+import { addToStatistics } from "./addToStatistics";
+import { saveData } from "./saveData";
+import { loadData } from "./loadData";
+
+export { addToStatistics, loadData, saveData };
