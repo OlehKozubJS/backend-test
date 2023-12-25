b00c496d6df19a50e1a9cc3844d099cfb3887e36
OlehKozubJS <oleg.kozub54@gmail.com>
2023-12-26 00:21:04 +0200

    Update index.js

--- operations/index.js
+++ operations/index.js
@@ -1,5 +1,6 @@
 import { addToStatistics } from "./addToStatistics";
 import { saveData } from "./saveData";
 import { loadData } from "./loadData";
+import { applicationRun } from "./applicationRun";
 
-export { addToStatistics, loadData, saveData };
+export { addToStatistics, loadData, saveData, applicationRun };
