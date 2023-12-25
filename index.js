e0d69324f30fe755bcd12471c44b9e77c952eba2
OlehKozubJS <oleg.kozub54@gmail.com>
2023-12-25 02:09:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const applicationRun = () => {
 const addToStatistics = async (request, response, next) => {
   try {
     const { url, method } = request;
-    const oldStatsData = await readFile("../messages.txt", "utf-8");
+    const oldStatsData = await readFile("./db/statistics.txt", "utf-8");
     const newStatsData = {
       date: new Date().toString(),
       url: url,
