3153557011640522da8ab693f5dddb3196543c02
OlehKozubJS <oleg.kozub54@gmail.com>
2023-12-27 00:25:55 +0200

    Update index.js

--- operations/index.js
+++ operations/index.js
@@ -2,5 +2,12 @@ const { addToStatistics } = require("./addToStatistics");
 const { saveData } = require("./saveData");
 const { loadData } = require("./loadData");
 const { applicationRun } = require("./applicationRun");
+const { removeData } = require("./removeData");
 
-module.exports = { addToStatistics, loadData, saveData, applicationRun };
+module.exports = {
+  addToStatistics,
+  loadData,
+  saveData,
+  removeData,
+  applicationRun,
+};
