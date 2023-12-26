03d9d42677bd9a71de9d40a4864b35d715fa9c2b
OlehKozubJS <oleg.kozub54@gmail.com>
2023-12-26 23:42:10 +0200

    add indexation

--- operations/saveData.js
+++ operations/saveData.js
@@ -5,9 +5,15 @@ const saveData = async (request, response, next) => {
     const oldDataString = await readFile("../messages.txt", "utf-8");
     const oldData = await JSON.parse(oldDataString);
     const body = await request.body;
+    let messageIndex = 0;
+    oldData.forEach((message) => {
+      if (messageIndex === message.messageIndex) {
+        messageIndex += 1;
+      }
+    });
     let newData;
     if (body.name && body.message) {
-      newData = [...oldData, body];
+      newData = [...oldData, { ...body, messageIndex }];
     } else {
       newData = [...oldData];
     }
