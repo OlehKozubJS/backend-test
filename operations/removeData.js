48a62a776ab9b43ab77f0f6b4bdbbb23a202e065
OlehKozubJS <oleg.kozub54@gmail.com>
2023-12-27 00:13:55 +0200

    Update removeData.js

--- operations/removeData.js
+++ operations/removeData.js
@@ -4,19 +4,7 @@ const saveData = async (request, response, next) => {
   try {
     const oldDataString = await readFile("../messages.txt", "utf-8");
     const oldData = await JSON.parse(oldDataString);
-    const body = await request.body;
-    let messageIndex = 0;
-    oldData.forEach((message) => {
-      if (messageIndex === message.messageIndex) {
-        messageIndex += 1;
-      }
-    });
-    let newData;
-    if (body.name && body.message) {
-      newData = [...oldData, { ...body, messageIndex }];
-    } else {
-      newData = [...oldData];
-    }
+    const params = await request.params;
     const newDataString = JSON.stringify(newData);
     await writeFile("../messages.txt", newDataString);
     response.send(body);
