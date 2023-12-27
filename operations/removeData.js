230455779b6cc62295fc3835988391932e403146
OlehKozubJS <oleg.kozub54@gmail.com>
2023-12-27 18:18:41 +0200

    set delete method

--- operations/removeData.js
+++ operations/removeData.js
@@ -2,9 +2,10 @@ const { writeFile, readFile } = require("fs/promises");
 
 const removeData = async (request, response, next) => {
   try {
+    /*
     const oldDataString = await readFile("../messages.txt", "utf-8");
     const oldData = await JSON.parse(oldDataString);
-    const params = await request.params;
+    const { params } = await request;
     const { messageIndex } = params;
     const newData = oldData.filter((message) => {
       message.messageIndex !== messageIndex;
@@ -17,6 +18,9 @@ const removeData = async (request, response, next) => {
     response.send(
       `deleed mesage: ${deletedMessage}, full data: ${newDataString}`
     );
+    */
+
+    response.send("<p>This is response from delte method</p>");
   } catch (error) {
     console.log(error.message);
     throw error;
