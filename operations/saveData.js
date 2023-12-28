b495a65b7fee67d199f342893080b4aeef7ac38f
OlehKozubJS <oleg.kozub54@gmail.com>
2023-12-28 22:34:35 +0200

    Update saveData.js

--- operations/saveData.js
+++ operations/saveData.js
@@ -3,19 +3,19 @@ const { writeFile, readFile } = require("fs/promises");
 const saveData = async (request, response, next) => {
   try {
     const oldDataString = await readFile("../messages.txt", "utf-8");
-    //const oldData = await JSON.parse(oldDataString);
+    const oldData = await JSON.parse(oldDataString);
     const body = await request.body;
     let messageIndex = 0;
-    oldDataString.forEach((message) => {
+    oldData.forEach((message) => {
       if (messageIndex === message.messageIndex) {
         messageIndex += 1;
       }
     });
     let newData;
     if (body.name && body.message) {
-      newData = [...oldDataString, { ...body, messageIndex }];
+      newData = [...oldData, { ...body, messageIndex }];
     } else {
-      newData = [...oldDataString];
+      newData = [...oldData];
     }
     const newDataString = JSON.stringify(newData);
     await writeFile("../messages.txt", newDataString);
