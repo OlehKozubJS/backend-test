5b7300c17db58435630e5d729a48de8bab7d5caf
OlehKozubJS <oleg.kozub54@gmail.com>
2023-12-29 15:04:24 +0200

    Update saveData.js

--- controllers/saveData.js
+++ controllers/saveData.js
@@ -1,7 +1,7 @@
 const { writeFile, readFile } = require("fs/promises");
 const { tryCatcher } = require("./tryCatcher");
 
-const saveData = async () => {
+const saveData = async (request, response, next) => {
   const data = readFile("../../messages.txt", "utf-8");
   response.send(data);
 };
